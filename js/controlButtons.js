183538ce03865b553a59e7abe65b5d680d924445
Thejan Rajapakshe <thejanr@codegen.net>
2018-01-15 11:43:20 +0530

    XMLHttpRequest

--- js/controlButtons.js
+++ js/controlButtons.js
@@ -2,37 +2,36 @@ var baseUrl = "http://159.203.181.233";
 var upApiCall = "/command/up";
 var stopApiCall = "/command/stop";
 var downApiCall = "/command/down";
-
-$("#up").click(function () {
-
-
-    console.log("Up");
-    controlFunctions("up");
-
-
-});
-
-$("#stop").click(function () {
-
-
-    console.log("Stop");
-    controlFunctions("stop");
-
-
-});
-
-$("#down").click(function () {
-
-
-    console.log("Down");
-    controlFunctions("down");
-
-
-});
-
+//
+// $("#up").click(function () {
+//
+//
+//     console.log("Up");
+//     controlFunctions("up");
+//
+//
+// });
+//
+// $("#stop").click(function () {
+//
+//
+//     console.log("Stop");
+//     controlFunctions("stop");
+//
+//
+// });
+//
+// $("#down").click(function () {
+//
+//
+//     console.log("Down");
+//     controlFunctions("down");
+//
+//
+// });
 
 function controlFunctions(func) {
-    "use strict";
+    // "use strict";
     var apiCall = "";
 
     if (func === "up") {
@@ -44,22 +43,18 @@ function controlFunctions(func) {
     }
 
 
-    $.ajax({
-        type: 'GET',
-        url: baseUrl + apiCall,
-        crossDomain: true,
-        dataType: "text",
-        jsonp: "jsoncallback",
-        jsonpCallback: "SMS",
-        cache: true,
-        success: function (data) {
-            console.log('Message Sent Successfully');
-        },
-        error: function (data) {
+    var url = baseUrl + apiCall;
 
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log('Message Sent Successfully');
+
+        } else if (this.readyState == 4) {
             console.error('ERROR: while dealing with function execution.');
 
-        }, timeout: 5000
-
-    });
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
 }
